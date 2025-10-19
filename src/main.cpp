e24baa7f2d36342633266a1b24bf3c52d83b03c3
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-10-20 00:21:36 +0200

    Drift Calculation Finished Completed

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(){
     std::cout << "Periodic Daily Returns: " << periodicDailyReturn << '\n';
 
 
-    std::vector<float> previous30days = {
+    std::vector<float> previous26days = {
         180.18, 182.23, 184.80, 184.77, 187.97, 193.51,
         192.23, 186.57, 186.23, 185.50, 189.19, 189.60,
         185.24, 182.08, 180.43, 178.17, 174.48, 179.77,
@@ -23,13 +23,17 @@ int main(){
         175.67, 177.77
     };
 
-    float average = mc::average(previous30days);
-    float variance = mc::variance(previous30days, average);    
+    float sampleMean = mc::average(previous26days);
+    float variance = mc::variance(previous26days, sampleMean);    
 
-    std::cout << "Average Daily Returns: " << average << '\n';
+    std::cout << "Sample Mean / Average Daily Returns: " << sampleMean << '\n';
     std::cout << "Variance: " << variance << "\n";
 
-    // float drift = mc::drift(averageDailyReturns, variance);
+    float averageDailyReturns = mc::average(previous26days);
+
+    float drift = mc::drift(averageDailyReturns, variance);
+
+    std::cout << "Drift: " << drift << '\n';
 
 
     return 0;
